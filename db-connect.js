d56527e4acba4fb061b86032152f727f47c2b0cc
kunal-chauhan4488 <chauhankunal447@gmail.in>
2026-03-25 13:00:20 -0700

    Testing

--- db-connect.js
+++ db-connect.js
@@ -1,9 +1,15 @@
-require("mongoose")
-    .connect("mongodb://localhost:27017/we_sept_25_crud")
-    .then(() => {
-        console.log("Database is connected");
-    })
-.catch (error => {
-    console.log(error);
-    
-})
+// require("mongoose")
+//     .connect("mongodb://localhost:27017/we_sept_25_crud")
+//     .then(() => {
+//         console.log("Database is connected");
+//     })
+// .catch (error => {
+//     console.log(error);
+
+// })
+
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("Database connected"))
+    .catch(err => console.log(err));
--- index.js
+++ index.js
@@ -1,10 +1,30 @@
-const express = require("express")
-const Router = require("./routes/Router")
-const hbs = require("hbs")
-const path = require("path")
-const app = express()
-require("./db-connect")
-app.set("view engine", "hbs")
-hbs.registerPartials(path.join(__dirname + "/views/partials"))
-app.use("/", Router)
-app.listen(8000, () => console.log(`Server is Running at http://localhost:8000`))
+// const express = require("express")
+// const Router = require("./routes/Router")
+// const hbs = require("hbs")
+// const path = require("path")
+// const app = express()
+// require("./db-connect")
+// app.set("view engine", "hbs")
+// hbs.registerPartials(path.join(__dirname + "/views/partials"))
+// app.use("/", Router)
+// app.listen(8000, () => console.log(`Server is Running at http://localhost:8000`))
+
+const express = require("express");
+const Router = require("./routes/Router");
+const hbs = require("hbs");
+const path = require("path");
+
+const app = express();
+
+require("./db-connect");
+
+app.set("view engine", "hbs");
+hbs.registerPartials(path.join(__dirname + "/views/partials"));
+
+app.use("/", Router);
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+    console.log(`Server is Running on port ${PORT}`);
+});
